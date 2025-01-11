f440dc051b631e2c8955b7940269d9efb6ab4886
Muhammad Ali Nizami <kustalics@gmail.com>
2025-01-11 11:37:59 +0500

    Auth Task Done

--- lib/views/register.dart
+++ lib/views/register.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_b6_v2/models/user.dart';
 import 'package:flutter_b6_v2/services/auth.dart';
+import 'package:flutter_b6_v2/services/user.dart';
 import 'package:flutter_b6_v2/views/login.dart';
 
 class RegisterView extends StatefulWidget {
@@ -13,6 +15,9 @@ class _RegisterViewState extends State<RegisterView> {
   TextEditingController emailController = TextEditingController();
 
   TextEditingController pwdController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
 
   bool isLoading = false;
 
@@ -25,13 +30,36 @@ class _RegisterViewState extends State<RegisterView> {
       body: Column(
         children: [
           TextField(
+            controller: nameController,
+            decoration: InputDecoration(label: Text("Name")),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: phoneController,
+            decoration: InputDecoration(label: Text("Phone")),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
+            controller: addressController,
+            decoration: InputDecoration(label: Text("Address")),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          TextField(
             controller: emailController,
+            decoration: InputDecoration(label: Text("Email")),
           ),
           SizedBox(
             height: 10,
           ),
           TextField(
             controller: pwdController,
+            decoration: InputDecoration(label: Text("Password")),
           ),
           SizedBox(
             height: 20,
@@ -59,28 +87,37 @@ class _RegisterViewState extends State<RegisterView> {
                           .signUpUser(
                               email: emailController.text,
                               password: pwdController.text)
-                          .then((val) {
-                        isLoading = false;
-                        setState(() {});
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return AlertDialog(
-                                title: Text("Message"),
-                                content: Text("User registered Successfully"),
-                                actions: [
-                                  TextButton(
-                                      onPressed: () {
-                                        Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    LoginView()));
-                                      },
-                                      child: Text("Okay"))
-                                ],
-                              );
-                            });
+                          .then((val) async {
+                        await UserServices()
+                            .createUser(UserModel(
+                                docId: val!.uid.toString(),
+                                email: emailController.text,
+                                address: addressController.text,
+                                name: nameController.text,
+                                phone: phoneController.text))
+                            .then((val) {
+                          isLoading = false;
+                          setState(() {});
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text("Message"),
+                                  content: Text("User registered Successfully"),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      LoginView()));
+                                        },
+                                        child: Text("Okay"))
+                                  ],
+                                );
+                              });
+                        });
                       });
                     } catch (e) {
                       isLoading = false;
